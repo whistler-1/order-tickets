044d8cc785f85e7b11b913cbcc49f9f28c1f5385
whistler-1 <baniel105@gmail.com>
2023-04-13 09:58:27 +0200

    Added sample data

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-//INSERT INTO Ticket (film,antall,fornavn,etternavn,telefonNr,epost)
-//VALUES ( 'Movie','1','A','B','213','1@2.com' );
-
---
+++ src/main/resources/sampleData.sql
@@ -0,0 +1,8 @@
+INSERT INTO TICKET (film,antall,fornavn,etternavn,telefonNr,epost)
+VALUES ( 'Superhero','1','Name','Last','213','1@2.com' );
+
+INSERT INTO TICKET (film,antall,fornavn,etternavn,telefonNr,epost)
+VALUES ( 'Spy Thriller','5','Jim','Smith','1-800-573','jsmith@mail.com' );
+
+INSERT INTO TICKET (film,antall,fornavn,etternavn,telefonNr,epost)
+VALUES ( 'Spy Thriller','2','Jane','Jones','19732','jjones@mail.com' );
